8185045fcff22c6d9d3aee11ede0d424e2b581ce
lovetwice1012 <lovetwice1012@gmail.com>
2021-12-08 10:17:54 +0900

    add MakeDatabase.php

---
+++ repo/MakeDatabase.php
@@ -0,0 +1,16 @@
+<?php
+$data = [];
+$result = file_get_contents("https://poggit.pmmp.io/plugins.json?latest-only");
+$result = json_decode($result,true);
+$i = 0;
+foreach ($result as $value) {
+    $data[$i] = [];
+    $data[$i]["name"] = $value["name"];
+    $data[$i]["version"] = $value["version"];
+    $data[$i]["artifact_url"] = $value["artifact_url"];
+    $data[$i]["api"] = $value["api"];
+    $data[$i]["deps"] = $value["deps"];    
+    $i = $i+1;
+}
+file_put_contents("Database.json",json_encode($data,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+echo "success";
