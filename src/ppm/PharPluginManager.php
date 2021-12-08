807da5e31796f0a9c0eeabca90208a36070780d3
lovetwice1012 <lovetwice1012@gmail.com>
2021-12-08 10:52:39 +0900

    uninstall修正？

--- src/ppm/PharPluginManager.php
+++ src/ppm/PharPluginManager.php
@@ -122,7 +122,7 @@ class PharPluginManager extends PluginBase implements Listener
                         }
                         $sender->sendMessage("インストールされているプラグインを検索中です");
                         $result = glob($this->getDataFolder()."plugins/*.phar");
-                        if(!in_array($args[1].".phar", $result)){
+                        if(!in_array("./".$args[1].".phar", $result)){
                             $sender->sendMessage("そのプラグインは現在インストールされていないようです");
                             return True;
                         }
@@ -164,7 +164,7 @@ class PharPluginManager extends PluginBase implements Listener
                         }
                         $sender->sendMessage("インストールされているプラグインを検索中です");
                         $result = glob($this->getDataFolder()."plugins/*.phar");
-                        if(!in_array($args[1].".phar", $result)){
+                        if(!in_array("./".$args[1].".phar", $result)){
                             $sender->sendMessage("そのプラグインは現在インストールされていないようです");
                             return True;
                         }
